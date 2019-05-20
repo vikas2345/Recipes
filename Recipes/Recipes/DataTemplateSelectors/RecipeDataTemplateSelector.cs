853b4ec12ac958b8acbf5ed185238ca78b9be266
Vikas <21singhvikas@gmail.com>
2019-05-20 18:32:17 +0530

    Implement DataTemplate Selector

---
+++ Recipes/Recipes/DataTemplateSelectors/RecipeDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using Recipes.Model;
+using Recipes.ViewCells;
+using Xamarin.Forms;
+
+namespace Recipes.DataTemplateSelectors
+{
+    public class RecipeDataTemplateSelector : DataTemplateSelector
+    {
+        readonly DataTemplate recipeTemplate;
+        readonly DataTemplate recommendedTemplate;
+
+        public RecipeDataTemplateSelector()
+        {
+            recipeTemplate = new DataTemplate(typeof(RecipeCell));
+            recommendedTemplate = new DataTemplate(typeof(RecommendedRecipeCell));
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var recipe = item as Recipe;
+            if (recipe == null)
+                return null;
+
+            return recipe.IsRecommended ? recommendedTemplate : recipeTemplate;
+
+        }
+    }
+}
