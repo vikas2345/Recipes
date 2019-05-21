ce23a11aa4523833615cf683d8be0cdb434b3395
Vikas <21singhvikas@gmail.com>
2019-05-21 17:37:07 +0530

    Added Dynamic Styles

--- Recipes/Recipes/Views/RecipeDetailPage.xaml.cs
+++ Recipes/Recipes/Views/RecipeDetailPage.xaml.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Recipes.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace Recipes.ViewCells
+namespace Recipes.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class RecommendedRecipeCell : ViewCell
+	public partial class RecipeDetailPage : ContentPage
 	{
-		public RecommendedRecipeCell ()
+		public RecipeDetailPage ()
 		{
 			InitializeComponent ();
 		}
+
+        public RecipeDetailPage(Recipe recipe)
+        {
+            InitializeComponent();
+        }
 	}
 }
--- Recipes/Recipes/Views/RecipePage.xaml.cs
+++ Recipes/Recipes/Views/RecipePage.xaml.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Recipes.DependencyService;
+using Recipes.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,16 @@ namespace Recipes.Views
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private void Recipe_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var recipeSelected = e.SelectedItem as Recipe;
+
+            if (recipeSelected == null)
+                return;
+
+            Navigation.PushAsync(new RecipeDetailPage(recipeSelected));
+
+        }
+    }
 }
